56a8773675353973985a5eba10663ab9f57707d5
Gabriel Menezes <gabriel.menezes3@hotmail.com>
2020-08-25 13:33:27 -0300

    Forma nutela de inserir os dados no .csv

--- parte-gabriel/appTCC/app.py
+++ parte-gabriel/appTCC/app.py
@@ -1,33 +1,23 @@
+# Chamando a API
 import requests
-import json
-import pandas as pd
-import csv
 
 url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-historical-data"
 
-# Buscando as ações diarias da petrobras referente ao periodo: 01/01/2019 até 31/12/2019
-# data convert to Epoch & Unix Timestamp
-querystring = {"frequency": "1d", "filter": "history",
-               "period1": "1546344000", "period2": "1577793600", "symbol": "PETR4.SA"}
+querystring = {"frequency":"1d","filter":"history","period1":"1546448400","period2":"1562086800","symbol":"AMRN"}
 
-# Passando os paremetros do get em 'headers'
-# Essa chave esta vinculada ao meu id
 headers = {
     'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
     'x-rapidapi-key': "5ff9fb4e8fmsh9d016c18191002fp1d1248jsn149d5adf2e0e"
-}
+    }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
 
-# print(response.text)
+# Forma nutela
+import pandas as pd
+import json 
 
-# Gerando o json
-# with open('financeData.json', 'w', encoding='utf-8') as f:
-#     json.dump(response.json(), f, ensure_ascii=False, indent=4)
+dados = response.json()
+df = pd.DataFrame(dados["prices"])
 
-
-# Transformando o json em arquivo .csv
-data = json.loads(response.text)
-
-for i in data["prices"]:
-    print(i["date"] + ';' + i["open"])
+#print(df.to_json(orient='split'))
+df.to_csv(r'teste.csv', index = None)
