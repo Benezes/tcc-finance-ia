60ba82d78910a229b83d3562bd85009e95a25433
Gabriel <gabriel.menezes3@Hotmail.com>
2021-03-28 16:08:38 -0300

    Update app.py
    adicionando a rede neural LSTM

--- parte-gabriel/app.py
+++ parte-gabriel/app.py
@@ -1,194 +1,142 @@
- 
-# Instalando as dependências
 '''
-%pip install requests
-%pip install pandas
-%pip install numpy
-%pip install install Quandl
-%pip install scikit-learn
+imports
 '''
- 
-# Importando as bibliotecas
-import requests
-import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+from sklearn.preprocessing import MinMaxScaler
 import numpy as np
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.linear_model import LinearRegression
-from sklearn.svm import SVR
-from sklearn.model_selection import train_test_split 
-import calendar
-import time
- 
+import pandas as pd
+import matplotlib.pyplot as plt
 
-
-
-# Consumindo os dados da API Yahoo Finance
-url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-historical-data"
- 
-# Passando a frequencia, o período e o nome da ação
-periodo1 = "1577841776" # 01/01/2020
-periodo2 = calendar.timegm(time.gmtime()) # Timestamp de hoje
-acao = "AMZN"
- 
-querystring = {"frequency": "1d", "filter": "history",
-               "period1": periodo1, "period2": periodo2, "symbol": acao}
- 
-key = ""
-headers = {
-    'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
-    'x-rapidapi-key': key
-}
- 
-response = requests.request("GET", url, headers=headers, params=querystring)
- 
-# Passando o retorno para uma variavel que transformara o retorno em um JSON
-dados = response.json()
- 
-# Criando um dataframe pegando apenas o objeto 'prices'
-df = pd.DataFrame(dados["prices"])
- 
-# Transformando o df['date'] que era um índice que estava em formato Unix timestamp em datetime
-df['date'] = pd.to_datetime(df['date'],unit='s')
- 
-# invertendo o df
-df = df.iloc[::-1]
- 
-# Data frame da data, open, high, low e close
-df_data = df.iloc[:,0:5]
-# Do DataFrame, o único dado que vamos usar é o close(fechamento)
-# data frame que vamos usar para fazer a previsão
-df = df.iloc[:,[4]]
- 
-# Definimos um dia X que queremos prever. No nosso caso 21 dias pra frente
-previsao_dias = 21
- 
-# Criando uma nova coluna 
-df['prediction'] = df[['close']].shift(-previsao_dias)
- 
 '''
-Imagine pegar os primeiros 21 dias de fechamento do mercado para fazer o algoritmo
-prever 21 dias pra frente
+Carregamento da base de dados
 '''
 
+# Importando a base de treinamento
+base = pd.read_csv('petr4_treinamento.csv')
 
-# Pegando todo o valor do fechamento menos os que vamos prever que definimos como 21 dias
-# *Criando um data set independente chamado X*
-# Tirando a coluna prediction do data set e transformando ele em um np_array
-X = np.array(df.drop(['prediction'],1))
- 
-# pegando todas os valores de Y menos as 21 últimas linhas
-X = X[:-previsao_dias]
+# Exlcuindo alguns valores nulos
+base = base.dropna()
 
+# Buscando os valores de fechamento do mercado
+base_treinamento = base.iloc[:, 4:5].values
+#print(base)
 
+# Aplicando uma normalização para colocar esses valores numa escala de 0 até 1
+normalizador = MinMaxScaler(feature_range=(0,1))
 
+# Normalizando uma base de treinamento. Range de 0 até 1
+base_treinamento_normalizada = normalizador.fit_transform(base_treinamento)
 
-# *Criando um data set independente chamado X*
-# Tirando a coluna prediction do df e transformando ele em um np_array
-Y = np.array(df['prediction'])
- 
-# pegando todas os valores de Y menos as 21 primeiras linhas
-Y = Y[:-previsao_dias]
+'''
+pré processamento da base de dados
+'''
 
+# Para saber quando que a ação ira valer hoje irei pegar 90 dias pra trás
+previsores = [] # aqui fica os 90 dias anteriores
+preco_real = []
 
-# Dividindo os valores dos dados
-# 80% dos dados é para treinamento
-# 20% dos dados é para teste
- 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
+# quantos preços devo pegar, quantidade de registros
+for i in range(90, 1242):
+    previsores.append(base_treinamento_normalizada[i-90:i, 0])# Adicionando os 90 registros anteriores em previsores[]
+    preco_real.append(base_treinamento_normalizada[i, 0])
 
+previsores, preco_real = np.array(previsores), np.array(preco_real) # Transformando a lista de previsores e preco real em numpy array
+previsores = np.reshape(previsores, (previsores.shape[0], previsores.shape[1], 1)) # Fazendo um redimencionamento na lista de previsores
 
-# Criando alguns modelos de treinamentos com alguns algoritmos
-# Modelo de treinamento com SVM
-svm = SVR(kernel='rbf', C=1e3, gamma=0.1)
-svm.fit(X_train, Y_train)
- 
-# Modelo de treinamento com Árvore de decisão regressiva
-tree = DecisionTreeRegressor().fit(X_train, Y_train)
- 
-# Modelo de treinamento com Regressão Linear
-lr = LinearRegression().fit(X_train, Y_train)
+'''
+construção da rede neural, LSTM, que tem  5 camadas
+'''
+
+es = EarlyStopping(monitor = 'loss', min_delta = 1e-10, patience = 10, verbose = 1) # Se passar 10 épocas sem melhorar o treinamento para de treinar
+rlr = ReduceLROnPlateau(monitor = 'loss', factor = 0.2, patience = 5, verbose = 1) # Reduz a métrica de aprendizagem quando para de melhorar
+mcp = ModelCheckpoint(filepath = 'pesos.h5', monitor = 'loss', save_best_only = True, verbose = 1) # Sempre salva o melhor modelo
+
+regressor = Sequential()
+
+# unit é neuronio
+# as camadas é oq fica entre a entrada e saida
+
+# Primeira camada
+regressor.add(LSTM(units = 200, return_sequences = True, input_shape = (previsores.shape[1], 1))) # Quanto mais units mais neuronio de memorias.
+regressor.add(Dropout(0.3))
+
+# Segunda camada
+regressor.add(LSTM(units = 150, return_sequences = True))
+regressor.add(Dropout(0.3))
+
+# Terceira camada
+regressor.add(LSTM(units = 150, return_sequences = True))
+regressor.add(Dropout(0.3))
+
+# Quarta camada
+regressor.add(LSTM(units = 150, return_sequences = True))
+regressor.add(Dropout(0.3))
+
+# Quinta camada
+regressor.add(LSTM(units = 150, return_sequences = True))
+regressor.add(Dropout(0.3))
+
+# Camada densa
+# é na camada densa que vamos achar  a solução da equação diferencial com o sigmoid
+regressor.add(Dense(units = 1, activation = 'sigmoid'))
+
+regressor.compile(optimizer = 'rmsprop', loss = 'mean_squared_error', metrics = ['mean_absolute_error'])
+
+# método fit irá treinar o modelo
+regressor.fit(previsores, preco_real, epochs = 100, batch_size = 32, callbacks = [es, rlr, mcp])
+
 
 
 '''
-Criando o Modelo de teste!: 
-A pontuação retorna o coeficiente de determinação R^2 da predição
-A melhor possibilidade de score/pontuação é 1.0
-Ou seja, quanto maior a pontuação melhor a confiança do algoritmo
+Previsão dos preços das ações
+preco_real_teste sera oque irei usar para comparar oque a previsão fez
 '''
-svm_confianca = svm.score(X_test, Y_test)
- 
-lr_confianca = lr.score(X_test, Y_test)
- 
-tree_confianca = tree.score(X_test, Y_test)
+base_teste = pd.read_csv('petr4_teste.csv')
+preco_real_teste = base_teste.iloc[:, 4:5].values # Queremos apenas o close
 
+base_completa = pd.concat((base['Close'], base_teste['Close']), axis = 0)  # Fazendo a extração dos 90 preços anteriores
 
-# Puxando apenas os valores a serem previstos
-previsto = np.array(df.drop(['prediction'],1))[-previsao_dias:]
+entradas = base_completa[len(base_completa) - len(base_teste) - 90:].values
+entradas = entradas.reshape(-1, 1)
+entradas = normalizador.transform(entradas)
 
+X_teste = []
+for i in range(90, 112):
+    X_teste.append(entradas[i - 90:i, 0])
 
+X_teste = np.array(X_teste)
+X_teste = np.reshape(X_teste, (X_teste.shape[0], X_teste.shape[1], 1))
 
-# Lista de previsão dos algoritmos
-svm_predict = svm.predict(previsto)
-lr_predict = lr.predict(previsto)
-tree_predict = tree.predict(previsto)
-
-
-# Lista que receberá a médias das previsões
-predicts_score = []
-
-
-# Média da SVM
-soma = 0
-for i in svm_predict:
-    soma += i
- 
-media_svm = soma / svm_predict.size
-predicts_score.append(media_svm)
-
-
-# Média da regressão linear
-soma = 0
-for i in lr_predict:
-    soma += i
- 
-media_lr = soma / lr_predict.size
-predicts_score.append(media_lr)
-
-
-# Média da tree predict 
-# Quanto tempo demora pra calcular essa média
-soma = 0
-for i in tree_predict:
-    soma += i
- 
-media_tree = soma / tree_predict.size
-predicts_score.append(media_tree)
+# Previsões 
+previsoes = regressor.predict(X_teste)
+previsoes = normalizador.inverse_transform(previsoes) # Desnormalizando
+qtd_previsoes = previsoes.size
 
 
 '''
-Todas as médias que foram inseridas na lista de predicts
-agora sera ordenada de maneira reversa para assim 
-pegarmos apenas o primeiro valor que é o score com melhor média.
-Em seguida pego o último fechamento da ação 
-para fazer uma comparação com a média.
+Comparações
 '''
-predicts_score.sort(reverse = True)
- 
-melhorMediaPredict = int(predicts_score[0])
- 
-primeiraData = df_data.tail(1)
-fechamentoDaPrimeiraData = primeiraData["close"]
-fechamento = int(fechamentoDaPrimeiraData)
+print('Previsoes')
+print(previsoes)
+print('\nPreço real')
+print(preco_real_teste)
+print("\n\n****************\n")
+print('Média entre a previsão e o preço real')
+print(f'Média das previsoes: {previsoes.mean()}')
+print(f'Média dos preços reais: {preco_real_teste.mean()}')
+print(f'Diferença de: {preco_real_teste.mean() - previsoes.mean()}')
 
 
+ '''
+Gráfico 
 '''
-0 = Se o fechamento estiver acima da média de 21 períodos = TENDÊNCIA DE ALTA
-1 = Se o fechamento estiver abaixo da média de 21 períodos = TENDÊNCIA DE BAIXA
-'''
-def mediaFechamento(fehamento, media):
-    if fehamento > media:
-        return 0
-    elif fechamento < media:
-        return 1
- 
-mediaFechamento(fechamento, melhorMediaPredict)
+plt.figure(figsize=(30,10))
+plt.plot(preco_real_teste, color = 'red', label = 'Preço real')
+plt.plot(previsoes, color = 'blue', label = 'Previsões')
+plt.title('Previsão preços das ações')
+plt.xlabel("Tempo")
+plt.ylabel("Valor")
+plt.legend()
+plt.show()
